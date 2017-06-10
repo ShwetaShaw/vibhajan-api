cb3e4ff0de9be5ec45baf394fdfbe167be807578
Shweta Shaw <shweta.shaw@coviam.com>
2017-06-10 17:45:09 +0530

    Fixing issue with controller invoking

--- src/main/java/com/lms/mobile/vibhajan/VibhajanApplication.java
+++ src/main/java/com/lms/mobile/vibhajan/VibhajanApplication.java
@@ -2,13 +2,14 @@ package com.lms.mobile.vibhajan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by shwetashaw on 10/06/17.
  */
 
 @SpringBootApplication (scanBasePackages = {"com.lms.mobile.vibhajan"})
-public class VibhajanApplication {
+public class VibhajanApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VibhajanApplication.class, args);
