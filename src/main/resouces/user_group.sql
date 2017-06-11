cde21f96f94dc0a7862d62816f668591ec22b901
Shweta Shaw <shweta.shaw@coviam.com>
2017-06-11 15:29:46 +0530

    Group APIS

--- src/main/resouces/user_group.sql
+++ src/main/resouces/user_group.sql
@@ -6,3 +6,16 @@ CREATE TABLE user_group
     WITH (
       OIDS=FALSE
     );
+
+    ALTER TABLE user_group ADD CONSTRAINT un_ug UNIQUE(user_id,group_id);
+
+    INSERT INTO public.user_group(
+                user_id, group_id)
+        VALUES (1, 11);
+
+    INSERT INTO public.user_group(
+                        user_id, group_id)
+                VALUES (11, 11);
+    INSERT INTO public.user_group(
+                        user_id, group_id)
+                VALUES (14, 11);
